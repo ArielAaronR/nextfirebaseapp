284c680fb9af44754bc1229e48c9c518bdcef0bf
Aaron Resurreccion <aaronresurreccion@gmail.com>
2022-03-26 15:54:27 -0700

    added images to public folder

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,48 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+export default function EnterPage(props) {
+  const user = null;
+  const username = null;
+
+  /**
+   * nested ternary
+   * 1. user signed out <SignInButton/>
+   * 2. user signed in, but missing username <UserNameForm/>
+   * 3. user signed in, has username <SignOutButton/>
+   */
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+// Sign in with Google Button
+function SignInButton() {
+  //Trigger a popup modal in the browser for the user to sign in and autheticate
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in With Google
+    </button>
+  );
+}
+
+// Sign out Button
+function SignOutButton() {}
+
+function UsernameForm() {}

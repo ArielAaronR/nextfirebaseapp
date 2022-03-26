3ba1a1ed488efb7946ca59c34f41af925987dae6
Aaron Resurreccion <aaronresurreccion@gmail.com>
2022-03-26 14:53:43 -0700

    learned about using toast npm package

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,12 @@ import Link from 'next/link';
 
 import Loader from '../components/Loader';
 
+import toast from 'react-hot-toast';
+
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>Toast Me</button>
     </div>
   );
 }
